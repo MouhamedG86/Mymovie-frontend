be9b07adfca8113e2f7d8df6854bc8e005663bdc
mouhamed gueye <gueyenioro2012@gmail.com>
2024-02-07 17:00:22 +0100

    op

--- components/Home.js
+++ components/Home.js
@@ -38,7 +38,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([])
 
   useEffect(() => {
-    fetch('https://mymoviz-backend-beta-three.vercel.app/movies')
+    fetch('https://mymovie-frontend-yfm3.vercel.app/movies')
       .then(response => response.json())
       .then(data => {
         setMoviesData(data.movies);
